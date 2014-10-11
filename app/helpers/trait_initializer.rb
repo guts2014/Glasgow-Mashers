23b3e70f68202f819157fe98bebf3eafc35d2fc3
Christian Shtarkov <cshtarkov@gmail.com>
2014-10-11 13:39:57 +0100

    Trait initialializer returns dictionary

--- app/helpers/trait_initializer.rb
+++ app/helpers/trait_initializer.rb
@@ -6,12 +6,12 @@ def initialize_traits
         {name: "Sloppy", mod: -5, likes: ["Lazy"], hates: ["Perfectionist"]},
         {name: "Lazy", mod: -3, likes: ["Sloppy"], hates: ["Perfectionist"]}
     ]
-    traits_list = []
+    traits_list = {}
     traits.each do |t|
         tobject = Trait.new t[:name], t[:mod] 
         t[:likes].each { |x| tobject.likes! x }
         t[:hates].each { |x| tobject.hates! x }
-        traits_list.push(tobject)
+        traits_list[tobject.name] = tobject
     end
     traits_list
 end       
