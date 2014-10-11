74a8186f3bd73a3944dfc68857508133a9c2271e
Christian Shtarkov <cshtarkov@gmail.com>
2014-10-11 15:45:14 +0100

    Person additions

--- app/helpers/person.rb
+++ app/helpers/person.rb
@@ -5,7 +5,7 @@ class Person
     def applyModifiers base
         baseInitial = base
         @modifiers.each do |m|
-            base += m.apply(@baseInitial)
+            base += m.apply(baseInitial)
         end
         base
     end
