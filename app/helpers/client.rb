ee867037f91a55f4713a0f189d1f967515c37bd5
Christian Shtarkov <cshtarkov@gmail.com>
2014-10-11 13:42:33 +0100

    Compatibility with new trait initializer

--- app/helpers/client.rb
+++ app/helpers/client.rb
@@ -1,4 +1,5 @@
 require("./person.rb")
+require("./trait_initializer.rb")
 
 AFFLUENCE_LOWER = 500
 AFFLUENCE_UPPER = 1000
@@ -7,16 +8,8 @@ class Client < Person
     attr_reader :affluence
 
     def initialize
-        # This will all be pulled from a DB
-        traitPerfectionist = Trait.new "Perfectionist", +5
-        traitPerfectionist.likes! "Perfectionist"
-        traitPerfectionist.hates! "Sloppy"
-        traitSloppy = Trait.new "Sloppy", -5
-        traitSloppy.likes! "Sloppy"
-        traitSloppy.hates! "Perfectionist"
-        traitLazy = Trait.new "Lazy", -2
-        traits = [traitPerfectionist, traitSloppy, traitLazy]
-
+        # This shouldn't be like that
+        traits = initialize_traits
         # Actual code
         @name = Faker::Name.name
         @trait = traits[rand(traits.length)]
--- app/helpers/staffmember.rb
+++ app/helpers/staffmember.rb
@@ -1,5 +1,6 @@
 require ("./person.rb")
 require ("./client.rb")
+require ("./trait_initializer.rb")
 require ("faker")
 
 BASE_PERFORMANCE_LOWER = 5
@@ -11,16 +12,8 @@ class StaffMember < Person
     attr_reader :happiness, :salary
 
     def initialize
-        # This will all be pulled from a DB
-        traitPerfectionist = Trait.new "Perfectionist", +5
-        traitPerfectionist.likes! "Perfectionist"
-        traitPerfectionist.hates! "Sloppy"
-        traitSloppy = Trait.new "Sloppy", -5
-        traitSloppy.likes! "Sloppy"
-        traitSloppy.hates! "Perfectionist"
-        traitLazy = Trait.new "Lazy", -3
-        traits = [traitPerfectionist, traitSloppy, traitLazy]
-
+        # This shouldn't be like that
+        traits = initialize_traits
         # Actual code
         @name = Faker::Name.name
         @trait = traits[rand(traits.length)]
