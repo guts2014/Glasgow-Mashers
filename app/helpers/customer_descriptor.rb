76ca2233005b7a38d01b4317926823e552190b3d
Christian Shtarkov <cshtarkov@gmail.com>
2014-10-11 15:50:03 +0100

    Customer leaving AI

--- app/helpers/customer_descriptor.rb
+++ app/helpers/customer_descriptor.rb
@@ -5,7 +5,8 @@ AFFLUENCE_LOWER = 500
 AFFLUENCE_UPPER = 1000
 
 class CustomerDescriptor < Person
-    attr_reader :affluence
+    attr_reader :affluence, :serviced_by
+    attr_writer :serviced_by
 
     def initialize
         # This shouldn't be like that
@@ -14,6 +15,11 @@ class CustomerDescriptor < Person
         @name = Faker::Name.name
         @trait = traits[traits.keys.sample]
         @affluence = [*AFFLUENCE_LOWER..AFFLUENCE_UPPER].sample
+        @serviced_by = nil
+    end
+
+    def leave?
+        (@affluence / 1000) > @serviced_by.performance + [*-50..50].sample
     end
 
 end    
